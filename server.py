d930a08a35d5f2ab14074b4a5a1ac339e4f6c289
Alexandre Beduc <alexandre.beduc@gmail.com>
2025-04-03 14:18:13 +0200

    Refactoring, separating functions that handle json loading from server functions and adding a main file as an entry point for the app

--- server.py
+++ server.py
@@ -1,24 +1,13 @@
-import json
-from flask import Flask,render_template,request,redirect,flash,url_for
-
-
-def loadClubs():
-    with open('clubs.json') as c:
-         listOfClubs = json.load(c)['clubs']
-         return listOfClubs
-
-
-def loadCompetitions():
-    with open('competitions.json') as comps:
-         listOfCompetitions = json.load(comps)['competitions']
-         return listOfCompetitions
+from utils import load_clubs, load_competitions
+from flask import Flask, render_template, request, redirect, flash, url_for
 
 
 app = Flask(__name__)
 app.secret_key = 'something_special'
 
-competitions = loadCompetitions()
-clubs = loadClubs()
+competitions = load_competitions()
+clubs = load_clubs()
+
 
 @app.route('/')
 def index():
