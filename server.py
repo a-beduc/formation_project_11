900ab53b567113417c053bf26ec9894ca7c1ade2
Alexandre Beduc <alexandre.beduc@gmail.com>
2025-04-11 21:02:39 +0200

    fix: Prevent clubs to use more than their points, or negative amount (green light) + When loading data from json, transform points and numberOfPlaces into type int. + Added a redirect to index if book isn't reached with the right parameters + Added a redirect to index or welcome if purchasePlaces isn't reached with the right parameters.

--- server.py
+++ server.py
@@ -4,14 +4,18 @@ from flask import Flask,render_template,request,redirect,flash,url_for
 
 def loadClubs():
     with open('clubs.json') as c:
-         listOfClubs = json.load(c)['clubs']
-         return listOfClubs
+        listOfClubs = json.load(c)['clubs']
+        for club in listOfClubs:
+            club['points'] = int(club['points'])
+        return listOfClubs
 
 
 def loadCompetitions():
     with open('competitions.json') as comps:
-         listOfCompetitions = json.load(comps)['competitions']
-         return listOfCompetitions
+        listOfCompetitions = json.load(comps)['competitions']
+        for comp in listOfCompetitions:
+            comp['numberOfPlaces'] = int(comp['numberOfPlaces'])
+        return listOfCompetitions
 
 
 app = Flask(__name__)
@@ -34,15 +38,20 @@ def showSummary():
     return render_template('welcome.html',club=club,competitions=competitions)
 
 
+@app.route('/book', defaults={'competition': '', 'club': ''}, strict_slashes=False)
+@app.route('/book/<competition>', defaults={'club': ''}, strict_slashes=False)
 @app.route('/book/<competition>/<club>')
 def book(competition,club):
-    foundClub = [c for c in clubs if c['name'] == club][0]
-    foundCompetition = [c for c in competitions if c['name'] == competition][0]
+    foundClub = next((c for c in clubs if c['name'] == club), None)
+    foundCompetition = next((c for c in competitions if c['name'] == competition), None)
     if foundClub and foundCompetition:
         return render_template('booking.html',club=foundClub,competition=foundCompetition)
-    else:
+    elif foundClub:
         flash("Something went wrong-please try again")
-        return render_template('welcome.html', club=club, competitions=competitions)
+        return render_template('welcome.html', club=foundClub, competitions=competitions), 404
+    else:
+        flash("Lost connection, please login")
+        return redirect(url_for('index'))
 
 
 @app.route('/purchasePlaces',methods=['POST'])
@@ -53,6 +62,38 @@ def purchasePlaces():
     competition['numberOfPlaces'] = int(competition['numberOfPlaces'])-placesRequired
     flash('Great-booking complete!')
     return render_template('welcome.html', club=club, competitions=competitions)
+    competition = next((c for c in competitions if c['name'] == request.form.get('competition', None)), None)
+    club = next((c for c in clubs if c['name'] == request.form.get('club', None)), None)
+
+    if club and competition:
+        try:
+            placesRequired = int(request.form.get('places', 0))
+        except ValueError:
+            placesRequired = 0
+        if placesRequired <= 0:
+            flash('You must enter a valid number of places')
+            return render_template('booking.html', club=club, competition=competition), 403
+
+        purchase_power = int(club.get('points', 0))
+        if purchase_power < placesRequired:
+            flash(f"You don't have enough points to proceed with your request. Requested : {placesRequired}, still allowed : {purchase_power}")
+            return render_template('booking.html', club=club, competition=competition), 403
+
+        competition_available = int(competition.get('numberOfPlaces', 0))
+        if competition_available < placesRequired:
+            flash(f"Not enough available places for this competition. Requested : {placesRequired}, still available : {competition_available}")
+            return render_template('booking.html', club=club, competition=competition), 403
+
+        competition['numberOfPlaces'] = int(competition['numberOfPlaces'])-placesRequired
+
+        flash('Great-booking complete!')
+        return render_template('welcome.html', club=club, competitions=competitions)
+    elif club:
+        flash("Something went wrong-please try again")
+        return render_template('welcome.html', club=club, competitions=competitions), 404
+    else:
+        flash("Lost connection, please login")
+        return redirect(url_for('index'))
 
 
 # TODO: Add route for points display
