82830e198a92c545c54394d3cb39c30b3d0cf453
Alexandre Beduc <alexandre.beduc@gmail.com>
2025-04-12 15:33:34 +0200

    test(integration): Add new integration tests for buying places (red light) + User must not be able to buy more than 12 places for a competition. + User must not be able to bypass maximum authorized by buying small amount multiple times (adding a cache for past transaction). + A cache must be updated to track purchases

--- tests/integration/test_purchase_places.py
+++ tests/integration/test_purchase_places.py
@@ -98,3 +98,83 @@ def test_purchase_places_more_than_competition_places(
             'Requested : 5, still available : 4')
             in html_response_purchase_places)
     assert 'Places available: 4' in html_response_purchase_places
+
+
+def test_purchase_7_places_twice_to_try_to_bypass_maximum(
+        client, mock_clubs, mock_competitions, mock_past_transaction):
+
+    assert mock_competitions[0]['numberOfPlaces'] == 25
+    assert mock_past_transaction == {}
+
+    book_url = "/book/Competition 001/Club 003"
+    response_book = client.get(book_url)
+    html_response_book = response_book.data.decode("utf-8")
+
+    assert response_book.status_code == 200
+    assert (f"Booking for {mock_competitions[0]['name']} || GUDLFT"
+            in html_response_book)
+
+    request_args = {'club': 'Club 003', 'competition': 'Competition 001',
+                    'places': '7'}
+    response_purchase_places = client.post('/purchasePlaces',
+                                           data=request_args)
+    html_response_purchase_places = (
+        response_purchase_places.data.decode("utf-8"))
+
+    assert response_purchase_places.status_code == 200
+    assert mock_competitions[0]['numberOfPlaces'] == 18
+    assert 'Great-booking complete!' in html_response_purchase_places
+    assert 'Number of Places: 18' in html_response_purchase_places
+    assert mock_past_transaction[('Competition 001', 'Club 003')] == 7
+
+    second_request_args = {'club': 'Club 003', 'competition': 'Competition 001',
+                    'places': '7'}
+    second_response_purchase_places = client.post('/purchasePlaces',
+                                           data=second_request_args)
+    second_html_response_purchase_places = (
+        second_response_purchase_places.data.decode("utf-8"))
+
+    assert second_response_purchase_places.status_code == 403
+    assert (f"Booking for {mock_competitions[0]['name']} || GUDLFT"
+            in second_html_response_purchase_places)
+    assert ('Your request exceed the maximum allowed. Requested : 7, still '
+            'allowed 5') in second_html_response_purchase_places
+
+
+def test_purchase_7_places_then_5_to_reach_maximum(
+        client, mock_clubs, mock_competitions, mock_past_transaction):
+    assert mock_competitions[0]['numberOfPlaces'] == 25
+    assert mock_past_transaction == {}
+
+    book_url = "/book/Competition 001/Club 003"
+    response_book = client.get(book_url)
+    html_response_book = response_book.data.decode("utf-8")
+
+    assert response_book.status_code == 200
+    assert (f"Booking for {mock_competitions[0]['name']} || GUDLFT"
+            in html_response_book)
+
+    request_args = {'club': 'Club 003', 'competition': 'Competition 001',
+                    'places': '7'}
+    response_purchase_places = client.post('/purchasePlaces',
+                                           data=request_args)
+    html_response_purchase_places = (
+        response_purchase_places.data.decode("utf-8"))
+
+    assert response_purchase_places.status_code == 200
+    assert mock_competitions[0]['numberOfPlaces'] == 18
+    assert 'Great-booking complete!' in html_response_purchase_places
+    assert 'Number of Places: 18' in html_response_purchase_places
+    assert mock_past_transaction[('Competition 001', 'Club 003')] == 7
+
+    second_request_args = {'club': 'Club 003',
+                           'competition': 'Competition 001',
+                           'places': '5'}
+    second_response_purchase_places = client.post('/purchasePlaces',
+                                                  data=second_request_args)
+    second_html_response_purchase_places = (
+        second_response_purchase_places.data.decode("utf-8"))
+
+    assert second_response_purchase_places.status_code == 200
+    assert 'Great-booking complete!' in second_html_response_purchase_places
+    assert mock_past_transaction[('Competition 001', 'Club 003')] == 12
