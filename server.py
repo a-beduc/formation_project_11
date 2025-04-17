46a7b3476970c791a43af0b19a00bd24a50cca92
Alexandre Beduc <alexandre.beduc@gmail.com>
2025-04-17 09:55:10 +0200

    cleanup: PEP8 + Comments + Function name in camelCase unchanged in case it was used by third party programs.

--- server.py
+++ server.py
@@ -1,25 +1,42 @@
 from datetime import datetime
 import json
-from flask import Flask,render_template,request,redirect,flash,url_for
+from flask import Flask, render_template, request, redirect, flash, url_for
 
 
 def loadClubs():
+    """
+    Extract data from a json file and return it as a list.
+    :return: a list of dictionaries containing club information
+    :rtype: List[Dict]
+    """
     with open('clubs.json') as c:
-        listOfClubs = json.load(c)['clubs']
-        for club in listOfClubs:
+        list_of_clubs = json.load(c)['clubs']
+        for club in list_of_clubs:
             club['points'] = int(club['points'])
-        return listOfClubs
+        return list_of_clubs
 
 
 def loadCompetitions():
+    """
+    Extract data from a json file and return it as a list.
+    :return: a list of dictionaries containing competition information
+    :rtype: List[Dict]
+    """
     with open('competitions.json') as comps:
-        listOfCompetitions = json.load(comps)['competitions']
-        for comp in listOfCompetitions:
+        list_of_competitions = json.load(comps)['competitions']
+        for comp in list_of_competitions:
             comp['numberOfPlaces'] = int(comp['numberOfPlaces'])
-        return listOfCompetitions
+        return list_of_competitions
 
 
 def is_competition_in_past(competition, date=None):
+    """
+    Check if competition is in the past and return a bool.
+    :param Dict competition: competition dictionary
+    :param datetime date: Optional date in case of a test using a fixed date
+    :return: A boolean indicating if competition is in the past
+    :rtype: bool
+    """
     time_directive = '%Y-%m-%d %H:%M:%S'
     time_competition = competition.get('date', None)
     date = date or datetime.today()
@@ -27,6 +44,14 @@ def is_competition_in_past(competition, date=None):
 
 
 def split_competitions_per_dates(competitions_list):
+    """
+    Extract the id of competition from a list and separate them in two
+    list based on the competition date.
+    :param List competitions_list:
+    :return: two lists containing int representing the index of
+             dictionaries found in the competitions list
+    :rtype: Tuple[List[int], List[int]]
+    """
     past_comps_ids, future_comps_ids = [], []
     date = datetime.today()
     for c_id, comp in enumerate(competitions_list):
@@ -37,20 +62,28 @@ def split_competitions_per_dates(competitions_list):
     return past_comps_ids, future_comps_ids
 
 
+# Initialize the Flask app.
 app = Flask(__name__)
 app.secret_key = 'something_special'
 
+# Initialize the application data.
 competitions = loadCompetitions()
 clubs = loadClubs()
 
+# Misc globals
 MAXIMUM_PLACES_AUTHORIZED = 12
 PAST_TRANSACTION = {}
-
 past_competitions_ids, future_competitions_ids = (
     split_competitions_per_dates(competitions))
 
 
 def get_split_competitions():
+    """
+    Use two comprehension list to get the competitions list for past
+    dates and future dates.
+    :return: two list of dictionaries containing competition information
+    :rtype: Tuple[List[Dict], List[Dict]]
+    """
     past_comps = [competitions[c_id] for c_id in past_competitions_ids]
     future_comps = [competitions[c_id] for c_id in future_competitions_ids]
     return past_comps, future_comps
@@ -58,92 +91,209 @@ def get_split_competitions():
 
 @app.route('/')
 def index():
+    """
+    Render the login page where users can enter their email to log in.
+    :return: Rendered HTML of the index page.
+    :rtype: str
+    """
     return render_template('index.html')
 
 
-@app.route('/showSummary',methods=['POST'])
+@app.route('/showSummary', methods=['POST'])
 def showSummary():
+    """
+    Display the welcome template after the user login.
+    :return: Rendered HTML of the summary page or the index page with a
+             401 status if login fails.
+    :rtype: str | Tuple[str, int]
+    """
     try:
-        club = [club for club in clubs if club['email'] == request.form.get('email')][0]
+        club = [club for club in clubs if club['email'] == request.form.get(
+            'email')][0]
     except IndexError:
         flash('The provided email is not valid.')
         return render_template('index.html'), 401
     past_competitions, future_competitions = get_split_competitions()
-    return render_template('welcome.html',club=club,past_competitions=past_competitions,future_competitions=future_competitions)
+    return render_template('welcome.html',
+                           club=club,
+                           past_competitions=past_competitions,
+                           future_competitions=future_competitions)
 
 
-@app.route('/book', defaults={'competition': '', 'club': ''}, strict_slashes=False)
-@app.route('/book/<competition>', defaults={'club': ''}, strict_slashes=False)
+@app.route('/book', defaults={'competition': '', 'club': ''},
+           strict_slashes=False)
+@app.route('/book/<competition>', defaults={'club': ''},
+           strict_slashes=False)
 @app.route('/book/<competition>/<club>')
-def book(competition,club):
-    foundClub = next((c for c in clubs if c['name'] == club), None)
-    foundCompetition = next((c for c in competitions if c['name'] == competition), None)
-    if foundClub and foundCompetition:
-        return render_template('booking.html',club=foundClub,competition=foundCompetition)
-    elif foundClub:
+def book(competition, club):
+    """
+    Display the booking page for a given competition and club.
+    :param str competition: Name of the competition
+    :param str club: Name of the club
+    :return: Booking page id both parameters are valid,
+             otherwise the welcome or the index page with appropriate
+             error/status.
+    :rtype: str | Tuple[str, int] | BaseResponse
+    """
+    found_club = next((c for c in clubs if c['name'] == club), None)
+    found_competition = next(
+        (c for c in competitions if c['name'] == competition), None)
+    if found_club and found_competition:
+        return render_template('booking.html',
+                               club=found_club,
+                               competition=found_competition)
+    elif found_club:
         flash("Something went wrong-please try again")
         past_competitions, future_competitions = get_split_competitions()
-        return render_template('welcome.html', club=foundClub, past_competitions=past_competitions,future_competitions=future_competitions), 404
+        return render_template('welcome.html',
+                               club=found_club,
+                               past_competitions=past_competitions,
+                               future_competitions=future_competitions), 404
     else:
         flash("Lost connection, please login")
         return redirect(url_for('index'))
 
 
-@app.route('/purchasePlaces',methods=['POST'])
+@app.route('/purchasePlaces', methods=['POST'])
 def purchasePlaces():
-    competition = next((c for c in competitions if c['name'] == request.form.get('competition', None)), None)
-    club = next((c for c in clubs if c['name'] == request.form.get('club', None)), None)
+    """
+    Verify the request and process the booking of places for a club if
+    conditions are met.
+    It can :
+        - Process request (process)
+        - Block request and render welcome.html (block)
+        - Block request and render booking.html (try again)
+        - Error (logout)
+
+    Validates constraints :
+        - Number of places requested
+          (Int between 0 and 12)
+        - Club's available points
+        - Remaining competition places
+        - Maximum quota of places per club per competition
+          (Verify past transactions)
+        - Competition date validity
+
+    :return: Rendered page with updated points, a status and a message.
+    :rtype: Tuple[str, int] | str | BaseResponse
+    """
+    competition = next((c for c in competitions if c['name'] ==
+                        request.form.get('competition', None)),
+                       None)
+    club = next((c for c in clubs if c['name'] ==
+                 request.form.get('club', None)),
+                None)
 
     if club and competition:
         try:
-            placesRequired = int(request.form.get('places', 0))
+            places_required = int(request.form.get('places', 0))
         except ValueError:
-            placesRequired = -1
-        if placesRequired == 0:
+            places_required = -1
+
+        # Zero place requested (process)
+        if places_required == 0:
             past_competitions, future_competitions = get_split_competitions()
             flash("You didn't buy any places")
-            return render_template('welcome.html', club=club, past_competitions=past_competitions, future_competitions=future_competitions), 200
-        if placesRequired < 0:
-            flash('You must enter a valid number of places')
-            return render_template('booking.html', club=club, competition=competition), 400
+            return render_template(
+                'welcome.html',
+                club=club,
+                past_competitions=past_competitions,
+                future_competitions=future_competitions), 200
 
-        quota_left = MAXIMUM_PLACES_AUTHORIZED - PAST_TRANSACTION.get((competition['name'], club['name']), 0)
+        # Negative or wrong type of places requested (try again)
+        if places_required < 0:
+            flash('You must enter a valid number of places')
+            return render_template(
+                'booking.html',
+                club=club,
+                competition=competition), 400
+
+        # Verify past transaction to update maximum authorized
+        quota_left = MAXIMUM_PLACES_AUTHORIZED - PAST_TRANSACTION.get(
+            (competition['name'], club['name']), 0)
+
+        # Club already bought the maximum places authorized (block)
         if quota_left <= 0:
-            flash(f"Your club has already bought {MAXIMUM_PLACES_AUTHORIZED} places for this competition. No more purchases allowed")
+            flash(f"Your club has already bought {MAXIMUM_PLACES_AUTHORIZED} "
+                  f"places for this competition. No more purchases allowed")
             past_competitions, future_competitions = get_split_competitions()
-            return render_template('welcome.html', club=club,past_competitions=past_competitions,future_competitions=future_competitions), 302
-        if placesRequired > quota_left:
-            flash(f"Your request exceed the maximum allowed. Requested : {placesRequired}, still allowed {quota_left}")
-            return render_template('booking.html', club=club, competition=competition), 400
+            return render_template(
+                'welcome.html',
+                club=club,
+                past_competitions=past_competitions,
+                future_competitions=future_competitions), 302
+
+        # Request would exceed maximum authorized (try again)
+        if places_required > quota_left:
+            flash(f"Your request exceed the maximum allowed. Requested : "
+                  f"{places_required}, still allowed {quota_left}")
+            return render_template(
+                'booking.html',
+                club=club,
+                competition=competition), 400
 
         purchase_power = int(club.get('points', 0))
-        if purchase_power < placesRequired:
-            flash(f"You don't have enough points to proceed with your request. Requested : {placesRequired}, still allowed : {purchase_power}")
-            return render_template('booking.html', club=club, competition=competition), 400
+
+        # Not enough club points (try again)
+        if purchase_power < places_required:
+            flash(f"You don't have enough points to proceed with your "
+                  f"request. Requested : {places_required}, still allowed : "
+                  f"{purchase_power}")
+            return render_template(
+                'booking.html',
+                club=club,
+                competition=competition), 400
 
         competition_available = int(competition.get('numberOfPlaces', 0))
-        if competition_available < placesRequired:
-            flash(f"Not enough available places for this competition. Requested : {placesRequired}, still available : {competition_available}")
-            return render_template('booking.html', club=club, competition=competition), 400
 
+        # Not enough competition places (try again)
+        if competition_available < places_required:
+            flash(f"Not enough available places for this competition. "
+                  f"Requested : {places_required}, still available : "
+                  f"{competition_available}")
+            return render_template(
+                'booking.html',
+                club=club,
+                competition=competition), 400
+
+        # Is competition in the past (block)
         if is_competition_in_past(competition):
             flash("This competition is already over.")
             past_competitions, future_competitions = get_split_competitions()
-            return render_template('welcome.html', club=club,past_competitions=past_competitions,future_competitions=future_competitions), 302
+            return render_template(
+                'welcome.html',
+                club=club,
+                past_competitions=past_competitions,
+                future_competitions=future_competitions), 302
 
-        competition['numberOfPlaces'] = int(competition['numberOfPlaces'])-placesRequired
-        club['points'] = int(club['points']) - placesRequired
+        # Process request (process)
+        competition['numberOfPlaces'] = (
+                int(competition['numberOfPlaces']) - places_required)
+        club['points'] = int(club['points']) - places_required
 
         PAST_TRANSACTION.setdefault((competition['name'], club['name']), 0)
-        PAST_TRANSACTION[(competition['name'], club['name'])] += placesRequired
+        PAST_TRANSACTION[(competition['name'], club['name'])] += (
+            places_required)
 
         flash('Great-booking complete!')
         past_competitions, future_competitions = get_split_competitions()
-        return render_template('welcome.html', club=club, past_competitions=past_competitions,future_competitions=future_competitions)
+        return render_template(
+            'welcome.html',
+            club=club,
+            past_competitions=past_competitions,
+            future_competitions=future_competitions)
+
+    # competition not found (block)
     elif club:
         flash("Something went wrong-please try again")
         past_competitions, future_competitions = get_split_competitions()
-        return render_template('welcome.html', club=club, past_competitions=past_competitions,future_competitions=future_competitions), 404
+        return render_template(
+            'welcome.html',
+            club=club,
+            past_competitions=past_competitions,
+            future_competitions=future_competitions), 404
+
+    # club not found (logout)
     else:
         flash("Lost connection, please login")
         return redirect(url_for('index'))
@@ -151,6 +301,11 @@ def purchasePlaces():
 
 @app.route('/board')
 def board():
+    """
+    Render the board page where non-logged users can see clubs points.
+    :return: Rendered HTML of the board page.
+    :rtype: str
+    """
     return render_template('board.html', clubs=clubs)
 
 
@@ -159,8 +314,15 @@ def board():
 
 @app.route('/logout')
 def logout():
+    """
+    Redirect the user to the login page.
+    :return: Redirect toward index.html
+    :rtype: BaseResponse
+    """
     return redirect(url_for('index'))
 
+
+# # Uncomment these lines if you want to launch the application.
 #
 # if __name__ == '__main__':
 #     app.run(debug=True)
